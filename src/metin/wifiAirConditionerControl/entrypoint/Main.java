c828b3d5f1ff4f9d254c012f78c6b2d12d9dba6a
AL989531 <AL989531@A00069248.ar.arcelik>
2015-07-22 10:15:12 +0300

    ilk deneme

---
+++ src/metin/wifiAirConditionerControl/entrypoint/Main.java
@@ -0,0 +1,20 @@
+package metin.wifiAirConditionerControl.entrypoint;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hrzafer
+ */
+public class Main {
+    
+    public static void main (String[] args){
+        
+        new EntryPoint().setVisible(true);
+        
+    }
+    
+}
